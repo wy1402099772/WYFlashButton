716992b2d21bd11689783c3a2ecb5850841e6e94
wy1402099772 <1402099772@qq.com>
2017-01-04 20:28:24 +0800

    Initial Commit

---
+++ WYFlashButton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WYFlashButton
+//
+//  Created by wyan assert on 2017/1/4.
+//  Copyright © 2017年 wyan assert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WYFlashButton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WYFlashButton
+//
+//  Created by wyan assert on 2017/1/4.
+//  Copyright © 2017年 wyan assert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
